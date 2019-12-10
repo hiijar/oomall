dfb0a32ad2e4198ca90ffd62638b35e8c3e70bdd
Florence-ouye <58393554+Florence-ouye@users.noreply.github.com>
2019-12-10 10:03:45 +0800

    Create GetUserGrouponVo.java

---
+++ src/main/java/xmu/oomall/VO/GetUserGrouponVo.java
@@ -0,0 +1,22 @@
+package xmu.oomall.controller.vo;
+
+public class GetUserGrouponVo {
+    private Integer userId;
+    private Integer showType;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getShowType() {
+        return showType;
+    }
+
+    public void setShowType(Integer showType) {
+        this.showType = showType;
+    }
+}
