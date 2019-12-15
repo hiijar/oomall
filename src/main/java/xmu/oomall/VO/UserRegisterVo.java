8f7c509b3096c81866a66186c05817bba987276d
Florence-ouye <58393554+Florence-ouye@users.noreply.github.com>
2019-12-15 11:51:38 +0800

    Create UserRegisterVo.java

---
+++ src/main/java/xmu/oomall/VO/UserRegisterVo.java
@@ -0,0 +1,57 @@
+package xmu.oomall.VO;
+
+/**
+ * 用户注册VO
+ * 
+ */
+public class UserRegisterVo {
+    private String username;
+    private String password;
+    private String telephone;
+    /**
+     * 手机验证码
+     */
+    private String code;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRegisterVo{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
